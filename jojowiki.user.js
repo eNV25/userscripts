1f7cc020efc5fd108eec7d94c14064f8771c914d
eNV25 <env252525@gmail.com>
2022-05-23 21:44:04 +0400

    Add jojowiki.user.js

---
+++ jojowiki.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        JoJo Wiki Redirector
+// @description Redirect jojo.fandom.com to jojowiki.com
+// @namespace   https://github.com/eNV25/userscripts
+// @homepageURL https://github.com/eNV25/userscripts
+// @match       https://jojo.fandom.com/wiki/*
+// @run-at      document-start
+// @grant       none
+// @noframes
+// @unwrap
+// ==/UserScript==
+
+document.location.replace(
+  "https://jojowiki.com" +
+    document.location.href.slice("https://jojo.fandom.com/wiki/".length)
+);
